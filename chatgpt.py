a9fe129d44d0134680d57fd96b92f9016f210092
saak00002 <saak00002@stud.uni-saarland.de>
2023-07-07 11:49:32 +0200

    updated chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -2,22 +2,21 @@ import openai
 
 
 def query(message):
-    openai.api_key = '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'
+    openai.api_key = ''
     messages = [{"role": "system", "content":
         "You are a intelligent assistant."}]
-
-    # you will add the information soon
-    if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-        reply = chat.choices[0].message.content
-        return reply
-    else:
-        return "Something is wrong with the message. "
-
-
-print(query("What is 2 + 2?"))
+    try:
+        # you will add the information soon
+        if message:
+            messages.append(
+                {"role": "user", "content": message},
+            )
+            chat = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo", messages=messages
+            )
+            reply = chat.choices[0].message.content
+            return reply
+        else:
+            return "Something is wrong with the message. "
+    except:
+        return "Language Model is not available now. Please try again later or develop your own model."
